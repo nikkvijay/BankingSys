e07c2aead69b5aab70392a0c0af7e2d550a38dbf
nikkvijay <80617036+nikkvijay@users.noreply.github.com>
2024-02-08 17:22:38 +0530

    Add files via upload

---
+++ Bank Managment System/src/bank/managment/system/Conn.java
@@ -0,0 +1,21 @@
+package bank.managment.system;
+
+import java.sql.*;
+
+public class Conn {
+	
+	Connection c;
+	Statement s;
+	
+	public Conn(){
+		try {
+			c = DriverManager.getConnection("jdbc:mysql:///bankmanagmentsystem", "root", "root");
+			s = c.createStatement();
+			
+		}catch (Exception e){
+			System.out.println(e);
+		}
+		
+	}
+
+}
